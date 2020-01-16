748d7d3d95c2a134f7509ec80d9f7a916e61d781
Anna Biga <aniabiga040896@gmail.com>
2020-01-16 13:40:06 +0100

    writing next lines of code

--- src/Homework.java
+++ src/Homework.java
@@ -3,5 +3,7 @@ public class Homework {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println("i\ncommitowania");
+
     }
 }
